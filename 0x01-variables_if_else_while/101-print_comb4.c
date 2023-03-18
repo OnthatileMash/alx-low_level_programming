dec878bea6b0b4b3f3f5738269634d4b44615578
OnthatileMash <Ojmashakoe@gmail.com>
2023-03-17 19:26:25 -0700

    The success combination in business is: Do what you do better... and: do more of what you do...

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a, b, c, counter = 0;
+
+	for (a = 48; a <= 55; a++)
+	{
+		b = 49 + counter;
+		for (; b <= 56; b++)
+		{
+			c = 50 + counter;
+			for (; c <= 57; c++)
+			{
+				putchar((char) a);
+				putchar((char) b);
+				putchar((char) c);
+				if (a < 56)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+		counter++;
+	}
+
+	putchar('\n');
+	return (0);
+}
