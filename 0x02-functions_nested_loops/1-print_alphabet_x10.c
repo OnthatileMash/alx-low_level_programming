c047dcd882ab8ebbe5c98bf448060bcca76e673f
OnthatileMash <Ojmashakoe@gmail.com>
2023-03-20 16:08:13 -0700

    Task 2

---
+++ 0x02-functions_nested_loops/1-print_alphabet_x10.c
@@ -0,0 +1,4 @@
+#include "main.h"
+
+void print_alphabet_x10(void)
+{
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints alphabets in lowercase ten times
+ *
+ * Return: All good
+ */
+
+void print_alphabet_x10(void)
+{
+	int a;
+	int i;
+
+	for (a = 0; a <= 10; a++)
+	{
+		for (i = 97; i <= 122; i++)
+		{
+			_putchar((char) i);
+		}
+	}
+	_putchar('\n');
+}
