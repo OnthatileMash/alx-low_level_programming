e8317a317c45504ca4615925344742479e6a96f5
OnthatileMash <Ojmashakoe@gmail.com>
2023-03-20 16:34:38 -0700

    Task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _islower - checks if character is a lower case
+ * @c: char type letter
+ * Return: 1 (lowercase) or 0 (not a lowercase)
+ */
+
+int _islower(int c)
+{
+	if (c <= 97 && c >= 122)
+		return (1);
+	else
+		return (0);
+
+}
