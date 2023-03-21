899dc3c52fd4bbb042a9c5ef71d1d514a7653862
OnthatileMash <Ojmashakoe@gmail.com>
2023-03-20 18:16:55 -0700

    Task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit of a number.
+ * @c: char type letter
+ * Return: prints the last digit of a number.
+ */
+
+int print_last_digit(int c)
+{
+	c %= 10;
+
+	_putchar(c);
+
+	return (c);
+}
