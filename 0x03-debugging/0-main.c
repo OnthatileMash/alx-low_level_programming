f22a279a04564e4457c58c698dde64727ebdf9e3
OnthatileMash <Ojmashakoe@gmail.com>
2023-03-23 16:20:25 -0700

    0x03-debugging Tasks

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
-* main - tests function that prints if integer is positive or negative
+* main - tests function that prints the correct output of 0
 * Return: 0
 */
 
 int main(void)
 {
-        positive_or_negative(0);
+	positive_or_negative(0);
 
-        return (0);
+	return (0);
 }
