702c9f12721768b6b8f0044aa16fa9a5951579fa
OnthatileMash <Ojmashakoe@gmail.com>
2023-03-24 17:23:34 -0700

    Task 8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -10,12 +10,11 @@ void print_square(int size)
 {
 	int a, b;
 
-	for (a = 1; a <= size; a++)
+	for (a = 0; a <= size; a++)
 	{
 		for (b = 1; b <= size; b++)
 			_putchar('#');
 		_putchar('\n');
 	}
-	_putchar('\n');
 }
 
