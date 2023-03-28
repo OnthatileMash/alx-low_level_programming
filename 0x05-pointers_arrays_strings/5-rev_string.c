b51da7182846600b91fc24839bc787ce68de57b6
OnthatileMash <Ojmashakoe@gmail.com>
2023-03-28 15:35:11 -0700

    Task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * rev_string -  reverses a string.
+ * @s: chat type pointer
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	char *start = s;
+	char *end = s;
+
+	for(; *end != '\0'; end++);
+	end--;
+
+	while (start < end)
+	{
+		char temp = *start;
+		*start = *end;
+		*end = temp;
+		start++;
+		end--;
+	}
+}
