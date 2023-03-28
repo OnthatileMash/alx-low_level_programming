973c038fdbeb8618ce950ddea76ec6a199b5ac42
OnthatileMash <Ojmashakoe@gmail.com>
2023-03-28 04:57:01 -0700

    Task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes a pointer to an int as parameter
+ * updates the value it points to to 98.
+ * @n: int type number
+ * @*n - int type point
+ * Reutrn: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
