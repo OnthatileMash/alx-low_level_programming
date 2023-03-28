db77e1133dcb9b85aa1ca2a7cf01d24ad587c08e
OnthatileMash <Ojmashakoe@gmail.com>
2023-03-28 15:54:24 -0700

    Task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _puts2 - prints every other character of a string.
+ * starting with the first character, followed by a new line.
+ * @s: chat type pointer
+ * Return: integer
+ */
+
+void puts2(char *s)
+{
+	int length = 0;
+
+	while (s[length] != '\0')
+	{
+		if (length % 2 == 0)
+		{
+			_putchar(s[length]);
+		}
+		length++;
+	}
+	_putchar('\n');
+}
