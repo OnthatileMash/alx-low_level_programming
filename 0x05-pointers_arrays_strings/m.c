f14c4e61d94122707209fc5f1db881992f164025
OnthatileMash <Ojmashakoe@gmail.com>
2023-03-28 19:41:38 -0700

    Task 9

---
+++ 0x05-pointers_arrays_strings/m.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies string pointer, with the null byte to a dest pointer
+ * @dest: char type string
+ * @src: char type string
+ * Return: chsr pointer
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	char arr_src[] = src;
+
+	dest = arr_src;
+
+	return (dest);
+}
